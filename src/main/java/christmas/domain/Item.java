bf1323ac36c9a9e30cdc70122f500727ddd52554
takoyakimchi <c4nyou@gmail.com>
2023-11-14 17:58:47 +0900

    feat: Item 클래스 작성 - 음식을 담는 객체

---
+++ src/main/java/christmas/domain/Item.java
@@ -0,0 +1,19 @@
+package christmas.domain;
+
+public class Item {
+    private String name;
+    private int price;
+    private FoodType type;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public FoodType getType() {
+        return type;
+    }
+}
