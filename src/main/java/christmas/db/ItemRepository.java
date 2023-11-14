3bcf92fbd8616187199d2f140b657a5bbab2a64d
takoyakimchi <c4nyou@gmail.com>
2023-11-14 23:58:54 +0900

    refactor(db): findByItem 로직을 stream으로 변경

--- src/main/java/christmas/db/ItemRepository.java
+++ src/main/java/christmas/db/ItemRepository.java
@@ -6,11 +6,9 @@ import java.util.Optional;
 public class ItemRepository {
 
     public static Optional<Item> findByName(String name) {
-        for (Item item : ItemData.itemData()) {
-            if (item.getName().equals(name)) {
-                return Optional.of(item);
-            }
-        }
-        return Optional.empty();
+        return ItemData.itemData()
+            .stream()
+            .filter(x -> x.getName().equals(name))
+            .findAny();
     }
 }
