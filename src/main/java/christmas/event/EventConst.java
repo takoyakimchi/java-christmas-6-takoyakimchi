50ef1590f3b4e611a60c6436ec7f3ddf53924b40
takoyakimchi <c4nyou@gmail.com>
2023-11-15 15:48:23 +0900

    refactor(event): 상수값에 해당 연도와 월의 마지막 날 추가(메서드 사용)

--- src/main/java/christmas/event/EventConst.java
+++ src/main/java/christmas/event/EventConst.java
@@ -3,5 +3,6 @@ package christmas.event;
 public class EventConst {
 
     public static final int EVENT_YEAR = 2023;
-    public static final int EVENT_MONTH = 12;
+    public static final int EVENT_MONTH = 4;
+    public static final int LAST_DAY_OF_EVENT_MONTH = CalendarChecker.getLastDayOfMonth();
 }
