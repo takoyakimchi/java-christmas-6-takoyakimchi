95ca3366f5c9fdfa46581438aca2c9f477bb4839
takoyakimchi <c4nyou@gmail.com>
2023-11-15 15:47:10 +0900

    fix(view): 상수값에 맞게 월 출력하도록 수정

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,5 +1,7 @@
 package christmas.view;
 
+import static christmas.event.EventConst.EVENT_MONTH;
+
 import camp.nextstep.edu.missionutils.Console;
 import christmas.db.ItemRepository;
 import christmas.item.Item;
@@ -12,7 +14,7 @@ public class InputView {
 
     public static Orders readDate() {
         while (true) {
-            System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+            System.out.println(EVENT_MONTH + "월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
             try {
                 String input = Console.readLine();
                 int orderDate = convertToInt(input, "유효하지 않은 날짜입니다. 다시 입력해 주세요.");
